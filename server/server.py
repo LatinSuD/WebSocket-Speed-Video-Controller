905a034465af8892624b02fdce41058d4013fa8f
LatinSuD <latinsud@gmail.com>
2022-10-19 18:11:34 +0200

    Include HTTP server function
    I forgot to add support for HTTP(s) server, so the same script can serve both Websocket and the HTML

--- server/server.py
+++ server/server.py
@@ -5,12 +5,31 @@ from websockets import serve
 from pprint import pprint
 import ssl
 from pathlib import Path
+from http import HTTPStatus
 
 sockets = {}
 PORT=8000
 
-# La parte externa de la funcion solo se invoca 1 vez por websocket
-async def echo(websocket, something):
+# Handles non-websockets requests
+async def handle_http(path, request_headers):
+    if "Upgrade" in request_headers:
+        return
+
+    response_headers = [
+        ('Server', 'Ad-hoc server'),
+        ('Connection', 'close'),
+    ]
+
+    if (path == "/websocket-speed-video-controller/" or path == "/websocket-speed-video-controller"):
+        response_headers.append(('Content-Type', "text/html"))
+        body = open("controller.html", 'rb').read()
+        return HTTPStatus.OK, response_headers, body
+    else:
+        return HTTPStatus.NOT_FOUND, response_headers, b"404. But who are you and what are you doing here?"
+
+
+# Handles a websocket connection during all its life
+async def handle_websocket(websocket, path):
     global sockets
 
     sockets[websocket]=None
@@ -27,14 +46,14 @@ async def echo(websocket, something):
             print("New player connected")
             pprint(sockets)
         else:
-            for ws in sockets:
+            for ws in list(sockets):
                 if sockets[ws] == "player":
                     try:
                         await ws.send(message)
                     except Exception as e:
                         # Websocket was probably closed. Mark as unused
                         print("Excepcion" + str(e))
-                        sockets[ws]=None
+                        del sockets[ws]
 
 
 async def main():
@@ -44,7 +63,7 @@ async def main():
             keyfile=Path(__file__).with_name('cert.key')
     )
 
-    async with serve(echo, "0.0.0.0", PORT, ssl=ssl_context):
+    async with serve(handle_websocket, "0.0.0.0", PORT, ssl=ssl_context, process_request=handle_http):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
